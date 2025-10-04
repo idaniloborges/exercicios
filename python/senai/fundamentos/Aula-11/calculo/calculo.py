476bdb7384a41c64b8057e25cd85ded6e2eba296
Danilo Borges <idaniloborges@gmail.com>
2025-10-04 10:32:50 -0300

    Update calculo.py

--- python/senai/fundamentos/Aula-11/calculo/calculo.py
+++ python/senai/fundamentos/Aula-11/calculo/calculo.py
@@ -1,7 +1,3 @@
-# 1. Crie um pacote chamado [calculo.py](http://calculo.py) e crie uma função para cada operação: +, -, *, /. Depois chame este pacote com o código ***import calculo*** .
-
-    # Após chame as funções uma a uma passando os parâmetros a e b com os devidos números para operar e veja o resultado
-
 def soma(a,b):
   return a + b
 
@@ -15,10 +11,7 @@ def div(a,b):
   return a / b
 
 if __name__ == "__main__":
-  a=1
-  b=3
-  print(soma(5,6))
-  print(sub(a,b))
-  print(mult(a,b))
-  print(div(a,b))
-
+  soma(a,b)
+  sub(a,b)
+  mult(a,b)
+  div(a,b)
