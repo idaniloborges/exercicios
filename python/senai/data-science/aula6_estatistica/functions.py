dab2dd6910be31826336cdc0223b8b799e29a1ae
Danilo Borges <idaniloborges@gmail.com>
2025-11-06 10:40:42 -0300

    Refactor statistics functions to return values
    Refactor statistical functions to return values instead of printing. Added functions for standard deviation and range calculations.

--- python/senai/data-science/aula6_estatistica/functions.py
+++ python/senai/data-science/aula6_estatistica/functions.py
@@ -1,49 +1,78 @@
 import statistics
 
-def media(conj_dados):
-    print(f'Média: {statistics.mean(conj_dados):.2f}') 
+# def media(conj_dados):
+#     print(f'Média: {statistics.mean(conj_dados):.2f}') 
+#     print('-------------')
 
-def moda(conj_dados):
-    verificacao = set(conj_dados)
-    if len(conj_dados) == len(verificacao):
-        print(f'Não há moda: {verificacao}')
-    else:
-        print(f'Moda: {statistics.mode(conj_dados)}')
+# def moda(conj_dados):
+#     verificacao = set(conj_dados)
+#     if len(conj_dados) == len(verificacao):
+#         print(f'Não há moda: {verificacao}')
+#     else:
+#         print(f'Moda: {statistics.mode(conj_dados)}')
 
-def mediana (conj_dados):
-    print(f'Mediana: {statistics.median(conj_dados)}')
+# def mediana (conj_dados):
+#     print(f'Mediana: {statistics.median(conj_dados)}')
 
 
-def analise_empresarial(conj_dados):
+# def analise_empresarial(conj_dados):
 
-    media = statistics.mean(conj_dados)
-    mediana = statistics.median(conj_dados)
-    verificacao = set(conj_dados)
-    variancia = statistics.variance(conj_dados)
-    desvio_padrao = statistics.stdev(conj_dados)
-    amplitude = max(conj_dados) - min(conj_dados)
+#     media = statistics.mean(conj_dados)
+#     mediana = statistics.median(conj_dados)
+#     verificacao = set(conj_dados)
+#     variancia = statistics.variance(conj_dados)
+#     desvio_padrao = statistics.stdev(conj_dados)
+#     amplitude = max(conj_dados) - min(conj_dados)
     
-    print(f'Média: {media:.2f}')
-    print(f'Mediana: {mediana}')
+#     print(f'Média: {media:.2f}')
+#     print(f'Mediana: {mediana}')
     
-    if len(conj_dados) == len(verificacao):
-        print(f'Não há moda: {verificacao}')
-    else:
-        print(f'Moda: {statistics.mode(conj_dados)}')
+#     if len(conj_dados) == len(verificacao):
+#         print(f'Não há moda: {verificacao}')
+#     else:
+#         print(f'Moda: {statistics.mode(conj_dados)}')
     
-    print(f'Variancia: {variancia:.2f}')
+#     print(f'Variancia: {variancia:.2f}')
 
-    print(f'Desvio padrão: {desvio_padrao:.2f}')
+#     print(f'Desvio padrão: {desvio_padrao:.2f}')
 
-    print(f'Amplitude: {amplitude:.2f}')
+#     print(f'Amplitude: {amplitude:.2f}')
 
 
 # Desafio 2
 
-def media(aluno):
-    media_notas = statistics.mean(aluno['notas'])
-    print(f'Média das notas: {media_notas}')
+def media(notas):
+    return statistics.mean(notas)
 
-def mediana(aluno):
-    mediana_notas = statistics.median(aluno['notas'])
-    print(f'Mediana das notas: {mediana_notas}')
+def mediana(notas):
+    return statistics.median(notas)
+
+def moda(notas):
+    notas_ordenadas = set(notas)
+    if len(notas_ordenadas) == len(notas):       
+        return 'Não há moda.'
+    else: 
+        return statistics.mode(notas)
+
+def desvio_padrao(notas):
+    return statistics.stdev(notas)
+
+def amplitude(notas):
+    return max(notas) - min(notas)
+
+def menor_nota(lista_alunos, notas):
+    alunos_nota_menor = []
+    alunos_nota_maior = []
+    menor_nota = min(notas)
+    
+    for aluno in lista_alunos:
+        for nota in aluno['notas']:
+            if nota == menor_nota:
+                alunos_nota_menor.append(aluno["nome"])
+                
+    print(alunos_nota_menor)
+    print(f'-------- Alunos com a menor nota {menor_nota} --------')
+    
+    for aluno in alunos_nota_menor:
+        print(f'- {aluno}')
+    
