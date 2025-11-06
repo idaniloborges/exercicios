23a3dcd315f2d595bde0f0b22e63087fcf96df76
Danilo Borges <idaniloborges@gmail.com>
2025-11-06 10:39:39 -0300

    Update desafio2.py

--- python/senai/data-science/aula6_estatistica/desafio2.py
+++ python/senai/data-science/aula6_estatistica/desafio2.py
@@ -1,18 +1,13 @@
 # ## ***Desafio 2***
 
- 
-
 # ***VOCÊ É UM DESENVOLVEDOR E PRECISA IMPLEMENTAR UM SISTEMA PARA UMA INSTITUIÇÃO DE ENSINO.***
-
-# ***O SISTEMA DEVE GERENCIAR AS NOTAS DOS ESTUDANTES, APRESENTANDO ESTATÍSTICAS COMO MODA, MÉDIA, MEDIANA, AMPLITUDE DESVIO PADRÃO. 
-# ALÉM DISSO, DEVE IDENTIFICAR A MENOR E A MAIOR NOTA. ORGANIZE O CÓDIGO EM MÓDULOS E SEPARE AS FUNCIONALIDADES EM FUNÇÕES DISTINTAS.***
-
+# ***O SISTEMA DEVE GERENCIAR AS NOTAS DOS ESTUDANTES, APRESENTANDO ESTATÍSTICAS COMO 
+# MODA, MÉDIA, MEDIANA, AMPLITUDE DESVIO PADRÃO. 
+# ALÉM DISSO, DEVE IDENTIFICAR A MENOR E A MAIOR NOTA. ORGANIZE O CÓDIGO EM MÓDULOS E SEPARE 
+# AS FUNCIONALIDADES EM FUNÇÕES DISTINTAS.***
 # ***1 - USAR STATISTICS***
-
 # ***2 - UTILIZE MÓDULOS SEPARADOS***
-
 # ***3 - Utilize Parâmetros, caso deixe mais flexível***
-
 # 4 - Extraia os dados de todos os alunos
 
 import statistics
@@ -41,11 +36,36 @@ lista_alunos = [
     },
 ]
 
+notas_lista = []
+notas = []
 media_geral = []
 
 for aluno in lista_alunos:
-    print(aluno['nome'])
-    functions.media(aluno)
-    functions.mediana(aluno)
-    
+    notas_lista.append(aluno['notas'])
+    media_geral.append(statistics.mean(aluno['notas']))
+    # print(aluno['nome'])
+    # functions.media(aluno)
+    # functions.mediana(aluno)
+
+for grupo_notas in notas_lista:
+    for nota in grupo_notas:
+        notas.append(nota)
+
+print(f'Média: {functions.media(notas):.2f}')
+print(f'Moda: {functions.moda(notas)}')
+print(f'Mediana: {functions.mediana(notas):.2f}')
+print(f'Desvio padrão: {functions.desvio_padrao(notas):.2f}')
+print(f'Amplitude: {functions.amplitude(notas):.2f}')
+print(functions.menor_nota(lista_alunos, notas))
+# print(f'Maior nota: {max(notas)}')
+
+
+# print(f'Média geral: {statistics.mean(notas):.2f}')
+# print(f'Mediana geral: {statistics.median(notas):.2f}')
+# print(f'Desvio padrão: {statistics.stdev(notas):.2f}')
+
+# print(notas)
+
+# print(media_geral)
+# print(statistics.mean(notas))
 # print(statistics.mean(lista_alunos[0]['notas']))
